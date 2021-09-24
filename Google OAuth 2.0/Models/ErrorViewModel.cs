6924f2dee2c339860bfff1979bccede27f4c36a1
samesh97 <Alahakoon123@>
2021-09-24 14:55:24 +0530

    Initial commit.

---
+++ Google OAuth 2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Google_OAuth_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
